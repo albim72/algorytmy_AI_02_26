0c9787b5dad1f1db8c4272e0e42abb0afabf9024
albim72 <marcin.albiniak72@gmail.com>
2026-03-02 13:32:09 +0100

    Update analiza_biegu.py

--- DZIEN_1/analiza_biegu.py
+++ DZIEN_1/analiza_biegu.py
@@ -6,17 +6,21 @@ checkpoints = [
     (102,"Start",0),
     (103,"Start",0),
     (104,"Start",0),
+    (105,"Start",0),
     (101,"Kościeliska",56),
     (102,"Kościeliska",67),
     (103,"Kościeliska",69),
     (104,"Kościeliska",23),
+    (105,"Kościeliska",45),
     (101,"Ornak",230),
     (102,"Ornak",267),
     (103,"Ornak",366),
     (104,"Ornak",88),
+    (105,"Ornak",80),
     (101,"Meta",428),
     (102,"Meta",546),
     (104,"Meta",145),
+    (105,"Meta",130),
 ]
 
 df = pd.DataFrame(checkpoints,columns=["bib","point","minute"])
@@ -30,3 +34,35 @@ total_times = (
     df[df["point"]=="Meta"].set_index("bib")["minute"]
 )
 print(total_times)
+
+#średnia prędkosc
+DIST_KM = 45
+avg_peed = DIST_KM / (total_times/60)
+print(avg_peed)
+
+#odchylenia std czasów
+
+mean_time = np.mean(total_times)
+std_time = np.std(total_times)
+print(f"średnie odchylenie: {std_time:.2f}, średni czas: {mean_time:.2f}")
+
+#wykrywanie anomalii
+z_scores = (total_times - mean_time) / std_time
+print(z_scores)
+
+#jeśli ktos ma |z| > 2 - statystycznie podejrzany
+outliers = total_times[np.abs(z_scores)>2]
+print(outliers)
+
+#analiza tempa między checkpointami
+df["delta"] = df.groupby("bib")["minute"].diff()
+print(df)
+
+#średnia długość segmentu
+segments_stats = df.groupby("point")["delta"].mean()
+print(segments_stats)
+
+#symulacja zmęczenia
+df["fatigue_index"] = np.log1p(df["minute"])
+print("_"*70)
+print(df)
